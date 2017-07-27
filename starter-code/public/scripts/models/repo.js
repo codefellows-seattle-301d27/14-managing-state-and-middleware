6d2c1726526e8a6ffda9b4fd62959221505c9dca
britinimalloy <britini.malloy@outlook.com>
2017-07-27 16:46:09 -0700

    answered all comments in repo.js

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -5,7 +5,8 @@ var app = app || {};
   const repos = {};
   repos.all = [];
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // COMMENT: DONE What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // (This function sends a request to github to get the user's repos and either gets them back or an error message.)
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
