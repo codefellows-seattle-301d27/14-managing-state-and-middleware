f085c0d4373793e438032a7da3a2f74fd9e56604
Alana Franklin <alanafrankly@gmail.com>
2017-07-27 16:22:19 -0700

    Finished repos.requestRepos comment

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,6 +6,7 @@ var app = app || {};
   repos.all = [];
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // RESPONSE: This function is doing a GET ajaz call to the backend to get the repose fromt he GitHub API. It is called by the aboutController. The only function that it calls it the callback function. When it is called, the callback is defined as app.repoView.index, which inits the about page with the github data.
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
