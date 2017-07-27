61ede4e4835dd78117da667c60e51d3e3c27ca67
Tyler W <tyler.walser@yahoo.com>
2017-07-27 16:00:03 -0700

    set up constring and npm install and github token

--- starter-code/server.js
+++ starter-code/server.js
@@ -8,7 +8,8 @@ const requestProxy = require('express-request-proxy'); // REVIEW: We've added a 
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = `postgres://postgres:${process.env.PG_PASSWORD}@localhost:5432/kilovolt`;
+// TODO: DONE Don't forget to set your own conString estimate 10 minutes actual 10 minutes
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
