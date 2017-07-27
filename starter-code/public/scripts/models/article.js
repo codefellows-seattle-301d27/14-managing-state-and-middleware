6ae35c1554c934ff38e5f9bdef59aa44473f76ab
Alana Franklin <alanafrankly@gmail.com>
2017-07-27 16:10:12 -0700

    Finsihed Article.loadAll comment

--- starter-code/public/scripts/models/article.js
+++ starter-code/public/scripts/models/article.js
@@ -14,6 +14,7 @@ var app = app || {};
   // REVIEW: With ES6 arrow functions, if the function only has one parameter, you don't need parentheses.
   //         This is similar to saying Article.loadAll = function(rows).
     // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+    // RESPONSE: This is the function that loads all of the article data into a usable variable. It sorts the data and then maps it to new Article elements, which are saved into Article.all
   Article.loadAll = rows => {
     rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
     Article.all = rows.map(ele => new Article(ele));
