6ad5ba9569d371b734bd761262c0b710e2e05b26
dmuldrew <dmuldrew@gmail.com>
2017-07-27 17:03:19 -0700

    added comment on numWordsByAuthor() call

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -5,7 +5,7 @@ var app = app || {};
   const repos = {};
   repos.all = [];
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
   // Makes a ajax call to get repo data. It is called in about controller. It calls the view method repoView.index in repoView.js as a callback.
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
