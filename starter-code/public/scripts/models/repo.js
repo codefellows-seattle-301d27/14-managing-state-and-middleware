06ba0d93757cbe27757fbd57589b6fe592f64ed2
jp <gaines.johnp@gmail.com>
2017-07-27 18:14:55 -0700

    finished more TODO's

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,6 +6,9 @@ var app = app || {};
   repos.all = [];
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+
+  /* This method has an anonymous function that has a callback as a parameter. Inside the function the get method is used on the /github/user/repos route.  The data retrieved is then set as a parameter in the repos.all method unless there is an error in which case console.error in invoked. Afterwards, the callback is invoked. This method is called in aboutController.index. DONE*/
+
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
