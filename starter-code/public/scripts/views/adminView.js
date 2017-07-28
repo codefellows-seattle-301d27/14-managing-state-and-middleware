c05f9cd347d0e580b9e7c4eab5758e890cc27dd3
dmuldrew <dmuldrew@gmail.com>
2017-07-27 17:11:12 -0700

    added render function comment

--- starter-code/public/scripts/views/adminView.js
+++ starter-code/public/scripts/views/adminView.js
@@ -5,7 +5,7 @@ var app = app || {};
   const adminView = {
     initAdminPage : () => {
       let template = Handlebars.compile($('#author-template').text());
-        // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+        // DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
       //  Gets number of words by author and is being called here. It calls the array methods Filter, Map, Reduce and  allAuthor() on Article which lives on the article.js.
       app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
       $('#blog-stats .articles').text(app.Article.all.length);
