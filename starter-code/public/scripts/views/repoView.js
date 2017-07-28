cb5cf8bc7ab3b9dfb4b5256fbaa5e7d78c566c2f
Sara Jade <wisconsinsara@gmail.com>
2017-07-27 17:35:13 -0700

    Comment repoView.index in repoView.js

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -13,7 +13,13 @@ var app = app || {};
 
   const render = Handlebars.compile($('#repo-template').text());
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // COMMENT: What is this function doing? Where is it called?
+  // Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // repoView.index() empties, shows, and hides via ui(). Then it appends mapped repos via app.repos()
+  // and renders them. It is called in aboutController.js and is used as a callback function
+  // app.repos.requestRepos in aboutController.js. It calls ui(), which lives in this file,
+  // append() and with() and map() which are built-in funcitons, and render(), which also lives in this file.
+  // est 5min act 10min
   repoView.index = function() {
     ui();
 
