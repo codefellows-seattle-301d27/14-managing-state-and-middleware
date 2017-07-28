11b93d53022bfce13f71f3ff39617b3736a2aefb
David Broxton <broxtondude@gmail.com>
2017-07-27 17:54:31 -0700

    Completes the last comments on the repoView.js file

--- starter-code/public/scripts/views/repoView.js
+++ starter-code/public/scripts/views/repoView.js
@@ -14,6 +14,7 @@ var app = app || {};
   const render = Handlebars.compile($('#repo-template').text());
 
   // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // This function renders the repo list by target sections in the html and appends the repos to the page. This function is being called in the aboutController.js file. This function also calls out the 'ui' function which is established above on this same document.
   repoView.index = function() {
     ui();
 
